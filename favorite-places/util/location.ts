a3adaf90c4e485a042c598b6f42970ce5f3fc661
Serhii Saveliev <saveliev.sergeyne@gmail.com>
2024-12-10 15:21:17 +0100

    Revert "fixed key"

--- favorite-places/util/location.ts
+++ favorite-places/util/location.ts
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "GOOGLE_API_KEY";
+const GOOGLE_API_KEY = "AIzaSyAjB6eWlRJuec2AQ8Yx8xt1uwaRBxPjrq4";
 
 export function getMapPreview(lat: number, lng: number) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
